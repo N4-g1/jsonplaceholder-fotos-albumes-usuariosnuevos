7697327963ea1818b1ef60f34ebdaba11d94f0be
N4-g1 <elguetadiegodiego@gmail.com>
2025-12-17 18:31:13 -0600

    Agregar el api_data y modificar el .gitignore

---
+++ auxiliares/api_data.py
@@ -0,0 +1,7 @@
+user = 'root'
+password = ''
+server = 'localhost'
+port = 3306
+database = 'jsonplaceholder_db'
+url_albums = 'https://jsonplaceholder.typicode.com/albums'
+url_photos = 'https://jsonplaceholder.typicode.com/photos'
